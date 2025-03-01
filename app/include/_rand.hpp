80ec751ad255f9dc7fdf2887bb4426a1910db577
Pawello09 <kontakt.pb.09@gmail.com>
2025-03-01 14:19:44 +0100

    Repaired linker errors

--- app/include/_rand.hpp
+++ app/include/_rand.hpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <vector>
 #include <cstdint>
+#include <stdexcept>
 
 class _rand {
 public:
@@ -17,3 +18,32 @@ public:
 private:
 	std::mt19937 gen;
 };
+
+/// <summary>
+/// [min, max] for integers, [min, max) for real
+/// </summary>
+template<typename T>
+T _rand::random(T min, T max) {
+	static_assert(std::is_arithmetic<T>::value, "bro randomizing letters or sum idk");
+
+	if (min > max) std::swap(min, max);
+
+	if constexpr (std::is_integral<T>::value) {
+		std::uniform_int_distribution<T> dist(min, max);
+		return dist(gen);
+	}
+
+	std::uniform_real_distribution<T> dist(min, max);
+	return dist(gen);
+}
+
+/// <summary>
+/// Returns a const ref. to a random element from a vector
+/// </summary>
+template<typename T>
+const T& _rand::choice(const std::vector<T>& container) {
+	if (container.empty()) throw std::invalid_argument("container as empty as my social life 😭");
+
+	std::uniform_int_distribution<size_t> dist(0, container.size() - 1);
+	return container[dist(gen)];
+}
