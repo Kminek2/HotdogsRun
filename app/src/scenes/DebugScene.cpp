62388b1a9f5c60296ee2a4955ad128d2a0ced3c6
Pawello09 <kontakt.pb.09@gmail.com>
2025-03-03 13:21:20 +0100

    Fixed direction of wheel rotation

--- app/src/scenes/DebugScene.cpp
+++ app/src/scenes/DebugScene.cpp
@@ -42,7 +42,7 @@ std::shared_ptr<Scene> DebugScene::Init() {
 	{
 		CarMovement* cmv = new CarMovement(1.0f, 1.0f, 600.0f, -100.0f, 100.0f, 20.0f, false);
 		objs[0]->AddScript(cmv);
-		objs[0]->AddScript(new WheelsScript(*cmv, "3x3_tire_1", 0.1f, 1.6f, 0.0f, 2.2f));
+		objs[0]->AddScript(new WheelsScript(*cmv, "3x3_tire_1", 0.9f, 0.9f, 0.0f, 2.2f));
 		objs[0]->AddScript(new CarInputs(*cmv));
 	}
 
