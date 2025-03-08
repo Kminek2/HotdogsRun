bcd39d5bb2748bbcff449efd1f8c697fd73721d1
Kamix-08 <pawlowski.kaka@gmail.com>
2025-03-08 18:28:57 +0100

    some racemanager shenanigans
    wip

--- app/src/objects/RaceManager.cpp
+++ app/src/objects/RaceManager.cpp
@@ -1,6 +1,7 @@
 #include "objects/RaceManager.hpp"
 
 #include <glm/vec3.hpp>
+#include <algorithm>
 
 const std::array<glm::vec2, 4> RaceManager::offsets = std::array<glm::vec2, 4>({ glm::vec2(1, 1), glm::vec2(-1, 1), glm::vec2(-1, -1), glm::vec2(1, -1) });
 
@@ -53,5 +54,59 @@ RaceManager* RaceManager::AddCar(GameObject* car) {
 	car->transform->Translate(glm::vec3(offset.x, offset.y, 0));
 
  	++cars_placed;
+	cars.push_back(car);
 	return this;
+}
+
+/// <summary>
+/// Start race. Has to have at least two cars and a termination condition.
+/// </summary>
+void RaceManager::StartRace() {
+	if (cars.size() < 2) throw std::invalid_argument("add more cars");
+	if (termination_condition == undefined) throw std::invalid_argument("define a condition");
+
+	car_objects.reserve(cars.size());
+	std::transform(cars.begin(), cars.end(), car_objects.begin(), [](GameObject* car) {return new CarObject(car, 0, 0); });
+
+	// add checkpoint handling logic here
+}
+
+/// <summary>
+/// Sets the termination condition.
+/// </summary>
+RaceManager* RaceManager::SetEndCondition(TerminationCondition condition, unsigned long long val) {
+	if (condition == undefined) throw std::invalid_argument("...really?");
+
+	termination_condition = condition;
+	termination_condition_value = val;
+
+	return this;
+}
+
+/// <summary>
+/// Force-end the race. If executeCallbacks is set to false, the subscribers will not be called
+/// </summary>
+RaceManager::CarObject* RaceManager::EndRace(bool executeCallbacks) {
+	if (!race_started) throw std::invalid_argument("race hasn't even started bruh");
+
+	std::sort(car_objects.begin(), car_objects.end(), [](CarObject* a, CarObject* b) {
+		if (a->lap == b->lap)
+			return a->time > b->time;
+		return a->lap > b->lap;
+	});
+
+	if (!executeCallbacks) 
+		return car_objects[0];
+
+	for (const auto& sub : subscribers)
+		sub();
+
+	return car_objects[0];
+}
+
+/// <summary>
+/// Subscribe a `void` return type function to the 'race end' event
+/// </summary>
+void RaceManager::SubscribeToRaceEnd(const std::function<void()>& callback) {
+	subscribers.push_back(callback);
 }
