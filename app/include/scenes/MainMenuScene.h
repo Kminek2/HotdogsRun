c2f69eaa8b07fa6055a715eaf9d01e7cec3c7e03
Pawello09 <kontakt.pb.09@gmail.com>
2025-03-20 22:44:35 +0100

    Map Selector update

--- app/include/scenes/MainMenuScene.h
+++ app/include/scenes/MainMenuScene.h
@@ -8,17 +8,24 @@ class MainMenuScene :
     public SceeneScript
 {
 private:
-    QuickCamera* qc;
+    //QuickCamera* qc;
     AnimationManager* am;
     std::vector<GameObject*> objs;
     static bool first_load;
     std::vector<std::pair<Text*, Text*>> menu_options;
+    std::vector<std::pair<Sprite*, std::pair<Text*, Text*>>> maps_options;
     int menu_choosen_option;
     void ShowMenu();
     void HideMenu();
     void UpdateMenu();
     void UpdateMenuHighlight();
     void first_animation();
+
+    void ShowMaps();
+    void UpdateMaps();
+    void UpdateMapsHighlight();
+    void HideMaps();
+
     void to_maps_animation();
     void from_maps_animation();
 public:
--- app/src/scenes/MainMenuScene.cpp
+++ app/src/scenes/MainMenuScene.cpp
@@ -5,6 +5,7 @@
 #include "Scene.h"
 #include "QuickCamera.h"
 #include "DebugPoints.h"
+#include "objects/CarMovement.h"
 
 #include "objects/Lights.h"
 #include "Sprite.h"
@@ -14,9 +15,9 @@ bool MainMenuScene::first_load = true;
 std::shared_ptr<Scene> MainMenuScene::Init() {
 	Scene* scene = new Scene(this);
 
-    qc = new QuickCamera();
-    qc->_sr(0.75f);
-	qc->_sm(100.0f);
+    //qc = new QuickCamera();
+    //qc->_sr(0.75f);
+	//qc->_sm(100.0f);
 
     Camera::main->cameraTransform->position = glm::vec3(-9.96294f, -9.06299f, 2.38608f);
     Camera::main->cameraTransform->rotation = glm::vec2(47.0f, -4.0f);
@@ -24,7 +25,10 @@ std::shared_ptr<Scene> MainMenuScene::Init() {
     objs.push_back(new GameObject());
     am = new AnimationManager();
     objs[objs.size()-1]->AddScript(am);
-
+    
+    objs.push_back(new GameObject("BaseCube", glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f), glm::vec3(1000.0f, 1000.0f, 0.0f)));
+    objs[objs.size()-1]->AddColorChange(glm::vec3(1.0f), glm::vec3(0.1f, 0.5f, 0.1f));
+    
     objs.push_back(new GameObject("stacjaBenzynowaCalosc"));
     objs.push_back(new GameObject("wjazd"));
     objs[objs.size()-1]->transform->RotateTo(glm::vec3(0.0f,0.0f,180.0f));
@@ -56,6 +60,8 @@ std::shared_ptr<Scene> MainMenuScene::Init() {
     objs.push_back(new GameObject("skrzyzowanieTrojkatAsfalt"));
     objs[objs.size()-1]->transform->Move(objs[objs.size()-2]->transform->position + glm::vec3(0.0f, 12.7f, -0.05f));
     objs[objs.size()-1]->transform->Rotate(glm::vec3(0.0f, 0.0f, -90.0f));
+    objs.push_back(new GameObject("f1car"));
+    objs[objs.size()-1]->AddScript(new CarMovement(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, false));
     
     if (first_load) {
         first_animation();
@@ -66,8 +72,8 @@ std::shared_ptr<Scene> MainMenuScene::Init() {
 }
 
 void MainMenuScene::Update() {
-    qc->HandleMove();
-    qc->HandleRotate();
+    //qc->HandleMove();
+    //qc->HandleRotate();
     if (!menu_options.empty())
         UpdateMenu();
 
@@ -153,6 +159,50 @@ void MainMenuScene::UpdateMenuHighlight() {
     menu_options[menu_choosen_option].first->ChangeSize(0.45f);
 }
 
+void MainMenuScene::ShowMaps() {
+    Sprite* sp = new Sprite("easy");
+    sp->rectTransform->SetWidth(0.25f);
+    sp->rectTransform->MoveTo(glm::vec3({-0.6f, 0.3f ,0.9f}));
+    Text* shadow = (new Text("SansSerif", {-0.79f,-0.31f,0.9f}, {-1,0}, 0.4f, glm::vec4(1.0f), -2.0f));
+    shadow->SetText("EASY");
+    shadow->SetColor(glm::vec4(glm::vec3(0.0f), 1.0f));
+    Text* text = (new Text("SansSerif", {-0.8f,-0.3f,0.8f}, {-1,0}, 0.4f, glm::vec4(1.0f), -2.0f));
+    text->SetText("EASY");
+    text->SetColor(glm::vec4(glm::vec3(0.0f, 0.7f, 0.0f), 1.0f));
+
+    sp = new Sprite("medium");
+    sp->rectTransform->SetWidth(0.25f);
+    sp->rectTransform->MoveTo(glm::vec3({0.0f, 0.3f ,0.9f}));
+    shadow = (new Text("SansSerif", {0.01f,-0.31f,0.9f}, {0,0}, 0.4f, glm::vec4(1.0f), -2.0f));
+    shadow->SetText("MEDIUM");
+    shadow->SetColor(glm::vec4(glm::vec3(0.0f), 1.0f));
+    text = (new Text("SansSerif", {0.0f,-0.3f,0.9f}, {0,0}, 0.4f, glm::vec4(1.0f), -2.0f));
+    text->SetText("MEDIUM");
+    text->SetColor(glm::vec4(glm::vec3(0.7f, 0.7f, 0.0f), 1.0f));
+
+    sp = new Sprite("hard");
+    sp->rectTransform->SetWidth(0.25f);
+    sp->rectTransform->MoveTo(glm::vec3({0.6f, 0.3f ,0.9f}));
+    shadow = (new Text("SansSerif", {0.81f,-0.31f,0.9f}, {1,0}, 0.4f, glm::vec4(1.0f), -2.0f));
+    shadow->SetText("HARD");
+    shadow->SetColor(glm::vec4(glm::vec3(0.0f), 1.0f));
+    text = (new Text("SansSerif", {0.8f,-0.3f,0.9f}, {1,0}, 0.4f, glm::vec4(1.0f), -2.0f));
+    text->SetText("HARD");
+    text->SetColor(glm::vec4(glm::vec3(0.7f, 0.0f, 0.0f), 1.0f));
+}
+
+void MainMenuScene::UpdateMaps() {
+
+}
+
+void MainMenuScene::UpdateMapsHighlight() {
+
+}
+
+void MainMenuScene::HideMaps() {
+
+}
+
 void MainMenuScene::first_animation() {
     am->addToQueue({{glm::vec3(-52.1495f, -18.5977f, 4.1227f), glm::vec2(2.0f, -7.0f)}, {glm::vec3(-19.5279f, -18.6894f, 4.55633f), glm::vec2(29.0f, -6.0f)}, 4.0f, {0.0f, 0.0f, 0.0f}});
     am->addToQueue({{glm::vec3(-19.5279f, -18.6894f, 4.55633f), glm::vec2(29.0f, -6.0f)}, {glm::vec3(-10.50373f, -12.5786f, 3.23154f), glm::vec2(40.0f, -7.0f)}, 2.0f, {0.0f, 0.0f, 0.0f}});
@@ -171,11 +221,19 @@ void MainMenuScene::to_maps_animation() {
     am->addToQueue({{glm::vec3(-5.16202f, 1.9114f, 1.5676f), glm::vec2(24.5f, -2.0f)}, {glm::vec3(6.91982f, 5.95392f, 2.12385f), glm::vec2(11.0f, -2.0f)}, 1.0f, {0.0f, 0.0f, 0.0f}});
     am->addToQueue({{glm::vec3(6.91982f, 5.95392f, 2.12385f), glm::vec2(11.0f, -2.0f)}, {glm::vec3(15.9145f, 5.02479f, 1.79929f), glm::vec2(62.0f, -7.25f)}, 1.5f, {0.0f, 0.0f, 0.0f}});
     am->addToQueue({{glm::vec3(15.9145f, 5.02479f, 1.79929f), glm::vec2(62.0f, -7.25f)}, {glm::vec3(16.8101f, 18.9178f, 1.79752f), glm::vec2(88.25f, 1.0f)}, 1.5f, {0.0f, 0.0f, 0.0f}});
+    am->addToQueue({{glm::vec3(16.8101f, 18.9178f, 1.79752f), glm::vec2(88.25f, 1.0f)}, {glm::vec3(16.8101f, 18.9178f, 1.79752f), glm::vec2(88.25f, 1.0f)}, 0.01f, {0.0f, 0.0f, 0.0f}, true, [](){}, [&](){
+        ShowMaps();
+    }});
 }
 
 void MainMenuScene::from_maps_animation() {
-    am->addToQueue({{glm::vec3(16.8101f, 18.9178f, 1.79752f), glm::vec2(88.25f, 1.0f)}, {glm::vec3(15.9145f, 5.02479f, 1.79929f), glm::vec2(62.0f, -7.25f)}, 1.5f, {0.0f, 0.0f, 0.0f}});
+    am->addToQueue({{glm::vec3(16.8101f, 18.9178f, 1.79752f), glm::vec2(88.25f, 1.0f)}, {glm::vec3(15.9145f, 5.02479f, 1.79929f), glm::vec2(62.0f, -7.25f)}, 1.5f, {0.0f, 0.0f, 0.0f}, false, [&](){
+        HideMaps();
+    }});
     am->addToQueue({{glm::vec3(15.9145f, 5.02479f, 1.79929f), glm::vec2(62.0f, -7.25f)}, {glm::vec3(6.91982f, 5.95392f, 2.12385f), glm::vec2(11.0f, -2.0f)}, 1.5f, {0.0f, 0.0f, 0.0f}});
     am->addToQueue({{glm::vec3(6.91982f, 5.95392f, 2.12385f), glm::vec2(11.0f, -2.0f)}, {glm::vec3(-5.16202f, 1.9114f, 1.5676f), glm::vec2(24.5f, -2.0f)}, 1.0f, {0.0f, 0.0f, 0.0f}});
     am->addToQueue({{glm::vec3(-5.16202f, 1.9114f, 1.5676f), glm::vec2(24.5f, -2.0f)}, {glm::vec3(-9.96294f, -9.06299f, 2.38608f), glm::vec2(47.0f, -4.0f)}, 1.0f, {0.0f, 0.0f, 0.0f}});
+    am->addToQueue({{glm::vec3(-9.96294f, -9.06299f, 2.38608f), glm::vec2(47.0f, -4.0f)}, {glm::vec3(-9.96294f, -9.06299f, 2.38608f), glm::vec2(47.0f, -4.0f)}, 0.01f, {0.0f, 0.0f, 0.0f}, true, [](){}, [&](){
+        ShowMenu();
+    }});
 }
