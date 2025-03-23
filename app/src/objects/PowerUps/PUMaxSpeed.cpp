826ce71425c12c77eb3230101474cd68f95ee57c
Kamix-08 <pawlowski.kaka@gmail.com>
2025-03-23 16:57:29 +0100

    add powerups disappearing

--- app/src/objects/PowerUps/PUMaxSpeed.cpp
+++ app/src/objects/PowerUps/PUMaxSpeed.cpp
@@ -1,9 +1,14 @@
 #include "objects/PowerUps/PUMaxSpeed.hpp"
 
 void PUMaxSpeed::OnCollide(Collisions::CollisionData* cd) {
+	std::cout << "pumaxspeed\n";
+
 	collided = (cd->obj1 == gameObject 
 		? cd->obj2 
 		: cd->obj1);
+
+	gameObject->transform->Move(glm::vec3(0, 0, -10));
+	Application::Invoke([this]() {gameObject->transform->Move(glm::vec3(0, 0, 10)); }, 5000);
 
 	OnActivate();
 }
--- app/src/objects/PowerUps/PUNitro.cpp
+++ app/src/objects/PowerUps/PUNitro.cpp
@@ -1,9 +1,14 @@
 #include "objects/PowerUps/PUNitro.hpp"
 
 void PUNitro::OnCollide(Collisions::CollisionData* cd) {
+	std::cout << "punitro\n";
+
 	collided = (cd->obj1 == gameObject
 		? cd->obj2
 		: cd->obj1);
+
+	gameObject->transform->Move(glm::vec3(0, 0, -10));
+	Application::Invoke([this]() {gameObject->transform->Move(glm::vec3(0, 0, 10)); }, 5000);
 
 	OnActivate();
 }
