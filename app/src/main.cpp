4d17e19c20148e28316b78ece76c93e6820842a4
Kminek2 <krzysztof_michalikkp@op.pl>
2025-04-01 20:54:23 +0200

    Removed terminal

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,17 +1,24 @@
 #include "Engine.h"
 #include <exception>
 
-#ifdef _CONSOLE
-int main() {
-#else
+//#ifdef _CONSOLE
+//int main() {
+//#else
 #ifdef _WIN32
 #include <Windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin, int nCmdShow){
+	HWND hwnd = GetConsoleWindow();
+	if (hwnd != NULL) {
+		ShowWindow(hwnd, SW_HIDE); // Hide the console
+}
+
 #else
-int main(int argc, char** argv){
-#endif //_WIN32
-#endif // Console
+int main(){
+#endif
+//int main(int argc, char** argv){
+//#endif //_WIN32
+//#endif // Console
 	Engine engine;
 	engine.Run();
 }
